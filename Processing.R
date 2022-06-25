50b97132126c0d54f1f872a7879b0ef6722ec128
luisamariarivera <luisa.rivera@emory.edu>
2022-06-25 16:41:37 +0200

    Add files via upload

--- Processing.R
+++ Processing.R
@@ -12,117 +12,86 @@ library(tmap)
 
 
 
-#you'll need to get an API token from the census.gov, each are individual
-
-census_api_key("a2b0bffcbfc22406b200e2626ade32194a0b60c9", install= TRUE)
- 
-#specify variables list
-
-vars<- c("B01003_001", "B03002_003", "B03002_004", "B19001_002", "B19001_003", "B19001_004", "B19001_005" , 
-         "B19001_014", "B19001_015", "B19001_016","B19001_017", "B19001A_014","B19001A_015", "B19001A_016", 
-         "B19001A_017", "B19001B_002", "B19001B_003", "B19001B_004" , "B19001B_005")
-
-#pull data from 2104 5 year ACS, specifying census tract level data for Shelby County TN
-
-shelby<- get_acs(geography = "tract", 
-              variables = vars,
-              state = "TN", 
-              county ="Shelby",
-              year = 2014,
-              output = "wide")
+p <- read_csv("data_raw/candle_psyc.csv")
+a <- read_csv("data_raw/aces_psle.csv")
+r<- read_csv("data_raw/PLAC_ages_RPC_clean.csv")
+c<-read_csv("data_raw/PLAC_ages_CPC_clean.csv")
+i<-read_csv("data_raw/newinc.csv")
+ct <- read_csv("data_raw/cell_type.csv")
 
 
-#calculate ICE income
-
-shelby$ICEr<- (shelby$B19001_014E+ shelby$B19001_015E+shelby$B19001_016E+ shelby$B19001_017E)
-shelby$ICEp<-(shelby$B19001_002E+shelby$B19001_003E+shelby$B19001_004E+shelby$B19001_005E)
-shelby$ICEinc<-((shelby$ICEr - shelby$ICEp)/shelby$B01003_001E)
-
-
-
-#calculate ICE race * income
-
-shelby$ICEwr<-(shelby$B19001A_014E + shelby$B19001A_015E + shelby$B19001A_016E + shelby$B19001A_017E)
-shelby$ICEbp<-(shelby$B19001B_002E + shelby$B19001B_003E + shelby$B19001B_004E + shelby$B19001B_005E)
-shelby$ICEraceinc<- ((shelby$ICEwr-shelby$ICEbp)/shelby$B01003_001E)
-
-#coerce to a dataframe
-
-d<-as.data.frame(shelby)
-
-
- #plot ICE Race * income 
-
-ggplot(d, aes(x = ICEraceinc, color=ICEraceinc)) +
-  geom_histogram( fill = "lightblue") + 
-  geom_vline(aes(xintercept = median(ICEraceinc)), linetype = "dashed") +
-  theme(panel.background = element_blank())+
-  xlab("Shelby County ICE Race x Income") +
-  ylab("Frequency")
-
-
-#Load a shape file for Shelby county census tracts
-
-
-shape<-tracts(47, county = 157, cb = TRUE, year = 2014)
-
-
-# convert the GEOID to numeric and join up datasets
-
-shape$GEOID <- as.numeric(shape$GEOID)
-d$GEOID<-as.numeric(d$GEOID)
-
-d<-merge(d, coi, by= GEOID, all.x=TRUE)
-
-
-spatial<-geo_join(shape, d, by = "GEOID", how = "left")
-ICEmap <- ggplot() + 
-  geom_sf(data = spatial, aes(fill = ICEraceinc))
-
- 
-ICEmap + labs(x= "Racial Wealth Inequality in Shelby County, TN", fill = "Concentration of White Wealth")+ theme(panel.background = element_blank())
-
-
-
-
-## Link ICE Variables to CANDLE Dataset (this was already done for you by UTHSC)
-## Load dataset
-
-setwd("~/Structural-Racism-and-Placental-DNA-Methylation/data_raw")
-
-library(readr)
-p <- read_csv("candle_psyc.csv")
-m<- read_csv("PLAC_ages_RPC_clean.csv")
-c<-read_csv("PLAC_ages_CPC_clean.csv")
 
 ##recode the ID to match naming convention for lab samples
 
 
-m$STUDYID<-as.numeric(m$STUDYID)
+r$STUDYID<-as.numeric(r$STUDYID)
 c$STUDYID<-as.numeric(c$STUDYID)
+i$STUDYID<-as.numeric(i$studyid)
+p$STUDYID <- as.numeric(p$STUDYID)
+a$STUDYID <- as.numeric(a$studyid)
+ct$STUDYID <- as.numeric(ct$sample_id)
 
-
-print(m$STUDYID)
-print(p$STUDYID)
-
+ 
 ##merge in data
 
-d1<-merge(m,p, by= "STUDYID")
+d1<-merge(p,i, by= "STUDYID")
 d2<-merge(d1, c, by="STUDYID")
+d3<-merge(d2, r, by="STUDYID")
+d4 <- merge (d3, a, by="STUDYID", all.x=TRUE)
+d5 <- merge(d4, ct, by="STUDYID", all.x=TRUE)
 
-d<-d2
-d<-merge(d,m,by="STUDYID")
-rm(d1,d2, c,p,m)
+d<-d5
+rm(d1,d2,d3,d4,d5,ct,c,p,r,i,a)
 
-##set variable labels, levels, etc
-d$residuals_RPC<-d$residuals_RPC.x
-d$DNAme_GA_RPC<-d$DNAme_GA_RPC.x
+##set variable names, labels, levels, etc
+
+d$aces1 <- d$MATERNAL_STRESS_Q1
+d$aces2 <- d$MATERNAL_STRESS_Q2
+d$aces3 <- d$MATERNAL_STRESS_Q3
+d$aces4 <- d$MATERNAL_STRESS_Q4
+d$aces5 <- d$MATERNAL_STRESS_Q5
+d$aces6 <- d$MATERNAL_STRESS_Q6
+d$aces7 <- d$MATERNAL_STRESS_Q7
+d$aces8 <- d$MATERNAL_STRESS_Q8
+d$aces9 <- d$MATERNAL_STRESS_Q9
+
+d$psle1 <- d$MATERNAL_STRESS_A
+d$psle2 <- d$MATERNAL_STRESS_B
+d$psle3 <- d$MATERNAL_STRESS_C
+d$psle4 <- d$MATERNAL_STRESS_D
+d$psle5 <- d$MATERNAL_STRESS_E
+d$psle6 <- d$MATERNAL_STRESS_F
+d$psle7 <- d$MATERNAL_STRESS_G
+d$psle8 <- d$MATERNAL_STRESS_H
+d$psle9 <- d$MATERNAL_STRESS_I
+d$psle10 <- d$MATERNAL_STRESS_J
+d$psle11 <- d$MATERNAL_STRESS_K
+d$psle12 <- d$MATERNAL_STRESS_L
+d$psle13 <- d$MATERNAL_STRESS_M
+d$psle14 <- d$MATERNAL_STRESS_N
+
+d$psle1_stress <- d$MATERNAL_STRESS_A_STRESS
+d$psle2_stress <- d$MATERNAL_STRESS_B_STRESS
+d$psle3_stress <- d$MATERNAL_STRESS_C_STRESS
+d$psle4_stress <- d$MATERNAL_STRESS_D_STRESS
+d$psle5_stress <- d$MATERNAL_STRESS_E_STRESS
+d$psle6_stress <- d$MATERNAL_STRESS_F_STRESS
+d$psle7_stress <- d$MATERNAL_STRESS_G_STRESS
+d$psle8_stress <- d$MATERNAL_STRESS_H_STRESS
+d$psle9_stress <- d$MATERNAL_STRESS_I_STRESS
+d$psle10_stress <- d$MATERNAL_STRESS_J_STRESS
+d$psle11_stress <- d$MATERNAL_STRESS_K_STRESS
+d$psle12_stress <- d$MATERNAL_STRESS_L_STRESS
+d$psle13_stress <- d$MATERNAL_STRESS_M_STRESS
+d$psle14_stress <- d$MATERNAL_STRESS_N_STRESS
+
+
+
 d$ICEincome<-as.numeric(d$ICEincome)
 d$ICErace<-as.numeric(d$ICErace)
 d$ICEraceinc<-as.numeric(d$ICEraceinc)
-d$age_difference_CPC<-d$DNAme_GA_CPC-d$GestAge
-d$age_difference_RPC<-d$DNAme_GA_RPC-d$GestAge
-d$abs_res_cpc<-abs(d$residuals_CPC)
+d$age_difference_CPC<-d$DNAme_GA_CPC-d$gestage
+d$age_difference_RPC<-d$DNAme_GA_RPC-d$gestage
 d$delivery<-substr(d$delclass,1,1)
 d$delivery<-as.factor(d$delivery)
 levels(d$delivery) <- c("Term", "Spontaneous preterm", "PROM preterm", "PROM c-section", "Preterm Fetal Ind", "Preterm  Maternal Ind") 
@@ -137,22 +106,25 @@ d$csex<-substr(d$csex, 1, 1)
 d$csex<-ifelse(d$csex=="1", 1,0)
 d$csex<-factor(d$csex,levels = c(0,1),
                labels = c("Female", "Male"))
+d$adjusted_income<-as.numeric(d$adjusted_income)
 d$minc<-factor(d$minc, levels =c(1,2,3,4,5,6,7,8,9,10,11),
                labels = c("<4.9k", "5-9.9k", "10-14.9k", "15-19.9k", "20-24.9k", "25-34.9k", "35- 44.9k", "45- 54.9k", "55 - 64.9k", "65-74.9k", ">75k"))
+
 d$meduc<-factor(d$meduc, levels=c(1,2,3,4,5), labels = c("<HS", "HS/GED", "Technical School","College", "Graduate/Professional"))
+d$hs <- ifelse(d$meduc== 1|d$meduc==2,1,0)
 d$mrace<-factor(d$mrace, levels= c(1,2,3), labels = c("Identifies as Black", "Identifies as White", "Does not identify as White or Black"))
+d$other_race<-d$`Other race`
+d$aces_pa <- d$tleq12
+d$aces_fv <- d$tleq13
+d$aces_sa <- d$tleq15
+d$worsttleq <- d$tleq21s
+
 ## select variables for analysis and save dataset
 
-setwd("~/Structural-Racism-and-Placental-DNA-Methylation/data cooked")
-
-library(dplyr)
-
 df<-d %>%
-  select(STUDYID, mage, mtotpreg, csex, meduc, minc, term, delivery, hypertension, diabetes, bmi, white, aa, asian, `Other race`, mhisp, mrace,gestage, 
-         residuals_CPC, residuals_RPC, acestot, psletot, tleqtot, ICErace, ICEraceinc, ICEincome )
+  select( mage, mtotpreg, csex, hs, term, delivery, hypertension, diabetes, bmi, white, aa, asian, other_race, mhisp, mrace,gestage, aces1, aces2, aces3, aces4, aces5, aces6, aces7, aces8, aces9,aces_sa, aces_fv, aces_pa, psle1,psle3,psle4,psle5,psle6,psle7, psle8,psle9,psle10,psle11,psle12,psle13,psle14, tleq1, tleq2, tleq3, tleq4, tleq5, tleq6, tleq7, tleq8, tleq9, tleq10,tleq11, tleq14, tleq16, tleq17, tleq18, tleq19, tleq20, worsttleq, ICErace, ICEraceinc, ICEincome, adjusted_income, age_difference_RPC, age_difference_CPC, Trophoblasts, Hofbauer,Endothelial,nRBC, Syncytiotrophoblast, CellType_PC1, CellType_PC2, CellType_PC3, CellType_PC4, CellType_PC5, meduc )
 
+write_csv(df, "data_cooked/df.csv")
 
-write.csv(df, "df.csv")
+rm(d)
 
-
-install.packages("mice")
